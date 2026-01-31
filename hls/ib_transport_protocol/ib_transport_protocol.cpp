ae3def9f70e5d0d4122cf80acf9e3fa08bac1cfc
Aaron Zuercher <zaaron@hacc-build-02.inf.ethz.ch>
2026-01-31 15:23:26 +0100

    changed timerready bool in fsm

--- hls/ib_transport_protocol/ib_transport_protocol.cpp
+++ hls/ib_transport_protocol/ib_transport_protocol.cpp
@@ -555,7 +555,8 @@ void rx_exh_fsm(
 		break;
 	case DMA_META:
 	//MT zaaron
-		if (!msnTable2rxExh_rsp.empty() && !udpLengthFifo.empty() && (!consumeReadInit || !retrans2rx_init.empty()) && (meta.op_code != RC_ACK || timer2flowcontrol.empty()))
+	bool timerready = (meta.op_code != RC_ACK) || !timer2flowcontrol.empty()
+		if (!msnTable2rxExh_rsp.empty() && !udpLengthFifo.empty() && (!consumeReadInit || !retrans2rx_init.empty()) && timerready)
 		{
 
 			msnTable2rxExh_rsp.read(dmaMeta);
